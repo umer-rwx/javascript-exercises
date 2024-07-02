a8069cfe1c1f2c6c749855ac720d675e2bf483f0
umer-rwx <umertariqsocial@gmail.com>
2024-07-02 13:54:47 +0500

    Done 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Getting the weird century numbers out of the way first
+    if ( year % 100 === 0 && year % 400 !== 0 ) {
+        return false;
+    }
+    // Now for the standard condition
+    if ( year % 4 == 0 ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
