cc2e5d08ef7cc2888a3dba00147c3cd002493f04
umer-rwx <umertariqsocial@gmail.com>
2024-07-02 12:53:02 +0500

    Done 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, reps) {
+    if (reps < 0) return "ERROR";
 
+    let returnString = "";
+    for (let i = 1; i <= reps; i++) {
+       returnString = returnString + string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
