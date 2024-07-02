8bec933e778a962ba30b35561c478b675655bb08
umer-rwx <umertariqsocial@gmail.com>
2024-07-02 13:04:02 +0500

    Done 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if (typeof string !== 'string') return 'ERROR';
+    let lastIndex = string.length - 1;
+    let returnString = "";
 
+    for (let i = lastIndex; i >= 0; i--) {
+        returnString = returnString + string.at(i);
+    }
+    return returnString;
 };
 
 // Do not edit below this line
