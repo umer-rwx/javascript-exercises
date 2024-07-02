36a0db09c74e208adc7fa8e8f87d600b24c39772
umer-rwx <umertariqsocial@gmail.com>
2024-07-02 13:34:36 +0500

    Done 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    // Outer loop iterates over all parameters after the array
+    // Inner loop iterates over all occurences of any given parameter until
+    // indexOf() returns -1 (no occurrence of the parameter)
+
+    for (let i = 1; i < arguments.length; i++) {
+        while ( array.indexOf(arguments[i]) !== -1 ) {
+            array.splice( array.indexOf(arguments[i]), 1 ) ;
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
