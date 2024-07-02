a4707fa9b8dd45f48ded88ca9181454c5fc4c1eb
umer-rwx <umertariqsocial@gmail.com>
2024-07-02 14:03:49 +0500

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = ( fahrenheit - 32 ) * 5 / 9;
+  return Math.round( celsius * 10 ) / 10; // Rounds to one decimal place;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = ( celsius * 9 / 5 ) + 32;
+  return Math.round( fahrenheit * 10 ) / 10; // Rounds to one decimal place;
 };
 
 // Do not edit below this line
