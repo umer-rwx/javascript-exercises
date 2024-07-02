f7a2eacd4912710f82f53fc4f663b6ece45806cc
umer-rwx <umertariqsocial@gmail.com>
2024-07-02 13:47:56 +0500

    Done 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Entry Checks
+    if ( a < 0 || b < 0 ) return 'ERROR'; 
+    if ( typeof a !== 'number' || typeof b !== 'number' ) return 'ERROR';
 
+    let sum = 0; 
+    let smaller = ( a < b ) ? a : b;
+    let larger = ( a > b ) ? a : b;
+
+    for ( let i = smaller; i <= larger; i++ ) {
+        sum = sum + i;        
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
