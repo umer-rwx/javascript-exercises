889b5b19f194baaf70065f9b087b1cc179f9bf18
umer-rwx <umertariqsocial@gmail.com>
2024-07-03 13:09:07 +0500

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const initialValue = 0;
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let initialValue = 1;
+  return arr.reduce((accumulator, currentValue) => accumulator * currentValue, initialValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0) return 1;
+  return x * factorial(x - 1);
 };
 
 // Do not edit below this line
