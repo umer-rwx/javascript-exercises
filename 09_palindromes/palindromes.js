c45dcc48afcf5263469538e62235ca186afd77a8
umer-rwx <umertariqsocial@gmail.com>
2024-07-03 14:01:41 +0500

    The numbers don't agree

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strLower = str.toLowerCase();
+    const arr = Array.from(strLower);
+    const forDeletion = [" ", ".", ",", "!"];
+    let arrClean = arr.filter((item) => !forDeletion.includes(item));
+    let arrCleanReverse = arrClean.reverse();
+    let strClean = arrClean.join("");
+    let strCleanReverse = arrCleanReverse.join("");
+    if (strClean === strCleanReverse) return true;
+    return false;
 };
 
 // Do not edit below this line
