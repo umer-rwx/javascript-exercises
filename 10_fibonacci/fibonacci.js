c66c2002e3657ff953b00800d65aeb89d6dffe75
umer-rwx <umertariqsocial@gmail.com>
2024-07-03 14:06:54 +0500

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) {
+        return "OOPS";
+    } else if (x == 1 || x == 2) {
+        return 1;
+    } else if (x == 0) {
+        return 0;
+    } else {
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
 };
 
 // Do not edit below this line
